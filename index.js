a6cf19283dde0443d8bf400fb7297b6812e78e4d
Franklin <fr@nkl.in>
2012-02-14 16:10:25 +0100

    geoipcity v1.0.0

---
+++ index.js
@@ -0,0 +1,25 @@
+var http = require('http');
+
+var lookup = function( license, ip, callback ) {
+	http.get({ host: 'geoip3.maxmind.com', port: 80, path: '/f?l='+ license +'&i='+ ip }, function( result ) {
+		result.setEncoding('utf8');
+		result.on('data', function( data ) {
+			callback( parseResult( data ) );
+		});
+	});
+}
+exports.lookup = lookup;
+
+var parseResult = function( str ) {
+	var result = [];
+	var head = ['countryCode', 'regionCode', 'city', 'postalCode', 'latitude', 'longitude', 'metroCode', 'areaCode', 'isp', 'org', 'extra'];
+	var str = str.split(',');
+	for( i=0; i<str.length; i++ ) {
+		if( str[i].substr(0,1) == '"' && str[i].substr(-1,1) == '"' ) {
+			str[i] = str[i].substr( 1, str[i].length -2 );
+		}
+		result[ head[i] ] = str[i];
+	}
+	return result;
+}
+exports.parseResult = parseResult;
